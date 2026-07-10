2037ba198b9de42a407b147552abf5db964abb72
Thulasi-Ram02 <thulasiramsiripireddy@gmail.com>
2026-07-10 23:21:14 +0530

    Update student_management.sql

--- sql/student_management.sql
+++ sql/student_management.sql
@@ -1,1 +1,16 @@
+CREATE DATABASE IF NOT EXISTS studentdb;
 
+USE studentdb;
+
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    course VARCHAR(50) NOT NULL
+);
+
+INSERT INTO students(name, age, course)
+VALUES
+('Rahul', 20, 'Java'),
+('Priya', 21, 'Python'),
+('Arjun', 22, 'SQL');
